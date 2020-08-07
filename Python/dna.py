008a1a9b8a114d1b78560204e3f9490d2bf993d0
ononoyun <ononcreates@gmail.com>
2020-08-07 17:49:19 +0800

    Update dna.py

--- Python/dna.py
+++ Python/dna.py
@@ -9,30 +9,33 @@ def main():
         print("Usage: file.csv file.txt")
         exit(1)
 
-# Open the CSV file for reading
-
+# Open the CSV file for reading and extract STR names from columns
     with open(argv[1],"r") as database:
         reader = csv.reader(database)
         i = next(reader)
+        sequences = {rows[0]:rows[len(i)-2] for rows in reader} # Need to figure out how to create a dictionary
+        print(sequences)
         for x in range (1, len(i), 1):
             print(f"{i[x]}")
-            check(i[x])
+            print(f"{count(i[x])}")
 
-def check(string):
+
+
+# Count STRs in the text file. Compute the longest run of consecitive repeats for each STR in the sequence
+def count(string):
     with open(argv[2],'r') as sequence:
         dna = sequence.read()
-        count = 0
+        consq = 0
         if string in dna:
             index = dna.find(string)
             print(index)
             if index > -1:
-                count = 1
+                consq = 1
             #compare the column name with DNA sequence
                 while dna[(index+len(string)) : (index+len(string)+len(string))] == string:
-                    count += 1
+                    consq += 1
                     index += len(string)
-                
-        print(count)
+        return consq
 
 main()
 #sequence = open(argv[2], "r")
@@ -56,7 +59,7 @@ main()
 
 # Open the text file
 
-# Count STRs in the text file. Compute the longest run of consecitive repeats for each STR in the sequence
+
 #sequence = {
 
 #}
